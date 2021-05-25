86c85fa442912cb8be2b9cb0a812f62584a660a3
Luke Cartey <5377966+lcartey@users.noreply.github.com>
2021-05-25 11:09:35 +0100

    Add InvalidFormat test

---
+++ InvalidFormat/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InvalidFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string s = string.Format("class {0} { }", "C");
+        }
+    }
+}
